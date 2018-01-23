cc91c8abcc354b41a92327b96c6806756225547e
Rushea Davis <rdavis@wlapcs.org>
2018-01-23 17:37:08 +0000

    added my name to the title

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
+var rusheaSlider, godelSlider, bobbiSlider;
+
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+    //createSlider(min, max, default)
+    rusheaSlider = createSlider(0, 255, 225);
+    rusheaSlider.position(20,20);
+    
+    godelSlider = createSlider(0, 255, 225);
+    godelSlider.position(20,50);
+    
+    bobbiSlider = createSlider(0, 255, 225);
+    bobbiSlider.position(20,80);
 }
 
 function draw(){
-    
+    redValue = rusheaSlider.value();
+    grenValue = godelSlider.value();
+    blueValue = bobbiSlider. value();
+    background(redValue, grenValue,blueValue);
 }
